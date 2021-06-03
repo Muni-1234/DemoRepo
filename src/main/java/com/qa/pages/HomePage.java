fe698b5cc1d244b4f083b30d2f5eb8c0ad29b317
Muni-1234 <munichandra.reddyk@gmail.com>
2021-06-03 12:05:18 +0200

    added driver initialize part

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,6 +1,10 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+	
+	WebDriver driver;
   
   public void test1() {
 		System.out.println("HomePage1 Test");
@@ -8,5 +12,9 @@ public class HomePage {
   
   public void test2() {
 		System.out.println("HomePage2 Test");
+	}
+  
+  public HomePage(WebDriver driver) {
+		this.driver = driver;
 	}
 }
